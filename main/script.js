b22512ed5704ca03513059335c04721e26bddf45
Krum Bezmenov <krum.bezmenov@gmail.com>
2024-11-01 12:14:29 +0200

    Creation class player and class game

--- main/script.js
+++ main/script.js
@@ -3,4 +3,46 @@ window.addEventListener("load", function () {
   const ctx = canvas.getContext("2d");
   canvas.width = 500;
   canvas.height = 500;
+
+  class Player {
+    constructor(game) {
+      this.game = game;
+      this.width = 120;
+      this.height = 190;
+      this.x = 20;
+      this.y = 100;
+      this.speedY = 0;
+    }
+    update() {
+      this.y += this.speedY;
+    }
+    draw(conext) {
+      conext.fillRect(this.x, this.y, this.width, this.height);
+    }
+  }
+
+  class Game {
+    constructor(widht, height) {
+      this.widht = widht;
+      this.height = height;
+      this.player = new Player(this);
+    }
+    update() {
+      this.player.update();
+    }
+    draw(context) {
+      this.player.draw(context);
+    }
+  }
+
+  const game = new Game(canvas.widht, canvas.height);
+
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    game.update();
+    game.draw(ctx);
+    requestAnimationFrame(animate);
+  }
+
+  animate();
 });
