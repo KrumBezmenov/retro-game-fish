5e1d1e0a63cd354edd05a8ff55c55eed3f091a9b
Krum Bezmenov <krum.bezmenov@gmail.com>
2024-11-16 08:41:45 +0200

    Adding class FireExplosion and code refactoring

--- main/script.js
+++ main/script.js
@@ -359,7 +359,12 @@ window.addEventListener("load", function () {
     constructor(game, x, y) {
       this.game = game;
       this.frameX = 0;
+      this.spriteWidth = 200;
       this.spriteHeight = 200;
+      this.width = this.spriteWidth;
+      this.height = this.spriteHeight;
+      this.x = x - this.width * 0.5;
+      this.y = y - this.height * 0.5;
       this.fps = 30;
       this.timer = 0;
       this.interval = 1000 / this.fps;
@@ -396,16 +401,12 @@ window.addEventListener("load", function () {
     constructor(game, x, y) {
       super(game, x, y);
       this.image = document.getElementById("smokeExplosion");
-      this.spriteWidth = 200;
-      this.width = this.spriteWidth;
-      this.height = this.spriteHeight;
-      this.x = x - this.width * 0.5;
-      this.y = y - this.height * 0.5;
     }
   }
 
   class FireExplosion extends Explosion {
     constructor(game, x, y) {
+      super(game, x, y);
       this.image = document.getElementById("fireExplosion");
     }
   }
@@ -609,7 +610,7 @@ window.addEventListener("load", function () {
 
     addExplosion(enemy) {
       const randomize = Math.random();
-      if (randomize < 1)
+      if (randomize < 0.5) {
         this.explosions.push(
           new SmokeExplosion(
             this,
@@ -617,6 +618,15 @@ window.addEventListener("load", function () {
             enemy.y + enemy.height * 0.5
           )
         );
+      } else {
+        this.explosions.push(
+          new FireExplosion(
+            this,
+            enemy.x + enemy.width * 0.5,
+            enemy.y + enemy.height * 0.5
+          )
+        );
+      }
     }
 
     checkCollision(rectOne, rectTwo) {
